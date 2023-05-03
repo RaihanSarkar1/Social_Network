4f9a533941aa46d1a42ff0ba2c8ccf01689ef70f
Raihan Sarkar <t.raihansarkar@gmail.com>
2023-05-03 18:35:47 +0600

    Adjusted the formatting of athe whole javascript code

--- network/static/network/javascript.js
+++ network/static/network/javascript.js
@@ -11,31 +11,31 @@ var staticUrl = DJANGO_STATIC_URL;
 
 
 // When the DOM loads load the posts by running the load function
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // event listener to call load if profile is clicked
     document.querySelector('#index').addEventListener('click', () => load('index'));
 
-    
 
-    
-    
-        
+
+
+
+
     // If there is the element profile_view in the DOM that means it is in the profile page
     if (document.getElementById("profile_view")) {
-        
+
         // Printing the URL
         console.log(window.location.href);
-        
+
         // Getting the username from the URL
         // get the url
         const url = window.location.href;
         const parts = url.split('/');
         const last_segment = parts.pop() || parts.pop();    // if the ending is / then first pop is empty so it will get the next string from second pop
-        
+
         console.log(last_segment);
 
         const username = last_segment;
-        
+
         // Load the posts of the user
         load(username);
 
@@ -49,34 +49,34 @@ document.addEventListener('DOMContentLoaded', function() {
             .then(response => response.json())
             .then(data => {
                 console.log(data)
-                if(data.SameUser === 'true') {
+                if (data.SameUser === 'true') {
                 }
-                else if(data.Following === 'true'){
+                else if (data.Following === 'true') {
                     document.querySelector('#btn_follow').style.display = 'none';
                     document.querySelector('#btn_unfollow').style.display = 'block';
                 } else {
                     document.querySelector('#btn_follow').style.display = 'block';
                     document.querySelector('#btn_unfollow').style.display = 'none';
                 }
-            
+
             })
-        
+
         //  eventlistener for follow button
         document.querySelector('#btn_follow').addEventListener('click', () => {
             console.log(username);
             fetch(`/pushFollow/${username}`)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
-                if(data.Following === 'true'){
-                    document.querySelector('#btn_follow').style.display = 'none';
-                    document.querySelector('#btn_unfollow').style.display = 'block';
-                } else {
-                    document.querySelector('#btn_follow').style.display = 'block';
-                    document.querySelector('#btn_unfollow').style.display = 'none';
-                }
-            
-            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.Following === 'true') {
+                        document.querySelector('#btn_follow').style.display = 'none';
+                        document.querySelector('#btn_unfollow').style.display = 'block';
+                    } else {
+                        document.querySelector('#btn_follow').style.display = 'block';
+                        document.querySelector('#btn_unfollow').style.display = 'none';
+                    }
+
+                })
 
         });
 
@@ -84,18 +84,18 @@ document.addEventListener('DOMContentLoaded', function() {
         document.querySelector('#btn_unfollow').addEventListener('click', () => {
             console.log(username);
             fetch(`/pushFollow/${username}`)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
-                if(data.Following === 'true'){
-                    document.querySelector('#btn_follow').style.display = 'none';
-                    document.querySelector('#btn_unfollow').style.display = 'block';
-                } else {
-                    document.querySelector('#btn_follow').style.display = 'block';
-                    document.querySelector('#btn_unfollow').style.display = 'none';
-                }
-            
-            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.Following === 'true') {
+                        document.querySelector('#btn_follow').style.display = 'none';
+                        document.querySelector('#btn_unfollow').style.display = 'block';
+                    } else {
+                        document.querySelector('#btn_follow').style.display = 'block';
+                        document.querySelector('#btn_unfollow').style.display = 'none';
+                    }
+
+                })
 
         });
 
@@ -125,8 +125,8 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         };
     }
-    
-    
+
+
 
 })
 
@@ -139,18 +139,18 @@ function load_follow_posts() {
     route = `/posts?start=${start}&end=${end}&user=null&follow=true`
 
     fetch(route)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        data.forEach(element => {
-            add_post(element);
-        });
-    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            data.forEach(element => {
+                add_post(element);
+            });
+        })
 }
 
 // load the posts
 function load(view) {
-    
+
     // Set the start and end post counters
     start = counter;
     end = counter + quantity - 1;
@@ -159,7 +159,7 @@ function load(view) {
     counter = end + 1;
 
     //Set the route according to the view
-    if (view === 'index'){
+    if (view === 'index') {
         // Default route to all posts
         route = `/posts?start=${start}&end=${end}&user=null`;
     }
@@ -169,15 +169,15 @@ function load(view) {
 
     // Get the posts and add an element post
     fetch(route)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        data.forEach(element => {
-            add_post(element);
-        });
-    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            data.forEach(element => {
+                add_post(element);
+            });
+        })
 
-    
+
 }
 
 // Redirects to the profile page of user
@@ -189,7 +189,7 @@ function load_profile(username) {
 }
 
 // Add a new post to the DOM
-function add_post(content){
+function add_post(content) {
 
     // invoke the get current user function
     get_current_user();
@@ -201,7 +201,7 @@ function add_post(content){
 
         //Create new post
         const post = document.createElement('div');
-        post.setAttribute('id','post');
+        post.setAttribute('id', 'post');
         post.classList.add("border");
         post.classList.add("rounded");
         post.classList.add("mb-2");
@@ -209,21 +209,21 @@ function add_post(content){
 
         // Add the profile pic
         const pic = new Image();
-        pic.src = staticUrl +  'static/img/user.png';
-        pic.setAttribute('id','pro_pic');
+        pic.src = staticUrl + 'static/img/user.png';
+        pic.setAttribute('id', 'pro_pic');
         post.appendChild(pic);
 
 
         // Add username of the post
         var post_user = document.createElement("h5");
-        post_user.innerHTML=username;
+        post_user.innerHTML = username;
         post_user.setAttribute('id', 'user_name');
         post_user.style.cursor = 'pointer';
 
         post.append(post_user);
 
         // Adding an event listener to detect a click on the username
-        post_user.addEventListener('click', function() {
+        post_user.addEventListener('click', function () {
             load_profile(username);
         })
 
@@ -231,23 +231,23 @@ function add_post(content){
         var current_user_id = localStorage.getItem('user_id');
         console.log(`The current user is ${current_user_id}`);
         // check if the current user is the owner of the post
-        if (content.user_id == current_user_id){
+        if (content.user_id == current_user_id) {
             // Add the delete icon
             const delete_icon = new Image();
-            delete_icon.setAttribute('id','delete');
+            delete_icon.setAttribute('id', 'delete');
             delete_icon.style.cursor = 'pointer'
-            delete_icon.src = staticUrl +  'static/img/delete.png';
+            delete_icon.src = staticUrl + 'static/img/delete.png';
             post.appendChild(delete_icon);
 
             // Add the edit icon
             const edit_icon = new Image();
-            edit_icon.setAttribute('id','edit');
+            edit_icon.setAttribute('id', 'edit');
             edit_icon.style.cursor = 'pointer'
-            edit_icon.src = staticUrl +  'static/img/edit.png';
+            edit_icon.src = staticUrl + 'static/img/edit.png';
             post.appendChild(edit_icon);
 
             // Adding an event listener to detect a click on the edit icon
-            edit_icon.addEventListener('click', function() {
+            edit_icon.addEventListener('click', function () {
 
                 edit_box = document.querySelector('dialog');
                 edit_box.showModal();
@@ -259,14 +259,16 @@ function add_post(content){
                 edit_box.addEventListener("click", e => {
                     const dialogDimensions = edit_box.getBoundingClientRect()
                     if (
-                      e.clientX < dialogDimensions.left ||
-                      e.clientX > dialogDimensions.right ||
-                      e.clientY < dialogDimensions.top ||
-                      e.clientY > dialogDimensions.bottom
+                        e.clientX < dialogDimensions.left ||
+                        e.clientX > dialogDimensions.right ||
+                        e.clientY < dialogDimensions.top ||
+                        e.clientY > dialogDimensions.bottom
                     ) {
-                      edit_box.close();
+                        edit_box.close();
                     }
-                  })
+
+                })
+
                 // edit_text.setSelectionRange(edit_text.value.length, edit_text.value.length);
 
                 // Adding an event listener to detect a click on the save button
@@ -290,33 +292,33 @@ function add_post(content){
                 //     })
 
                 // })
-                
-                                       
-                
+
+
+
             })
 
             // Adding an event listener to detect a click on the delete icon
-            delete_icon.addEventListener('click', function() {
+            delete_icon.addEventListener('click', function () {
                 fetch(`/delete/${content.id}`)
-                .then(res => res.json())
-                .then(response => {
-                    console.log(response);
-                    if (response.message == "deleted"){
-                        post.remove();
-                    }
-                })
+                    .then(res => res.json())
+                    .then(response => {
+                        console.log(response);
+                        if (response.message == "deleted") {
+                            post.remove();
+                        }
+                    })
             })
         }
-        
 
-        
+
+
 
 
 
 
         // Add the content
         var post_text = document.createElement("span");
-        post_text.innerHTML=content.text;
+        post_text.innerHTML = content.text;
         post_text.setAttribute('id', 'user_text');
         post.append(post_text);
 
@@ -332,61 +334,61 @@ function add_post(content){
 
         });
 
-        
-        
+
+
         // Create an element to show date time
         const createdDate = document.createElement('p');
-        createdDate.setAttribute('id','date');
+        createdDate.setAttribute('id', 'date');
 
         createdDate.innerHTML = `${formattedDateTime}`;
         post.appendChild(createdDate);
 
         // Add the heart
         const img = new Image();
-        img.setAttribute('id','heart');
+        img.setAttribute('id', 'heart');
 
         // Create the count
         const count = document.createElement('div');
         count.setAttribute('id', 'count');
 
         fetch(`/checkLike/${content.id}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            if (data.message == "likes"){
-                img.src = staticUrl +  'static/img/hearted.png';
-                count.innerHTML= data.count;
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data.message == "likes") {
+                    img.src = staticUrl + 'static/img/hearted.png';
+                    count.innerHTML = data.count;
 
-            }
-            else{
-                img.src = staticUrl +  'static/img/heart.png';
-                count.innerHTML= data.count;
-            }
+                }
+                else {
+                    img.src = staticUrl + 'static/img/heart.png';
+                    count.innerHTML = data.count;
+                }
 
-        })
-        
+            })
+
         post.appendChild(img);
         post.appendChild(count);
 
-        
 
 
-        img.addEventListener('click', function() {
+
+        img.addEventListener('click', function () {
             fetch(`/like/${content.id}`)
-            .then(res => res.json())
-            .then(response => {
-                console.log('Success:', response.message, response.count);
-                if (response.message === "liked") {
-                    img.src = staticUrl + 'static/img/hearted.png';
-                    count.innerHTML= response.count;
-                }
-                else {
-                    img.src = staticUrl +  'static/img/heart.png'; 
-                    count.innerHTML= response.count;
-                } 
+                .then(res => res.json())
+                .then(response => {
+                    console.log('Success:', response.message, response.count);
+                    if (response.message === "liked") {
+                        img.src = staticUrl + 'static/img/hearted.png';
+                        count.innerHTML = response.count;
+                    }
+                    else {
+                        img.src = staticUrl + 'static/img/heart.png';
+                        count.innerHTML = response.count;
+                    }
 
-            })
-            .catch(error => console.error('Error:', error));
+                })
+                .catch(error => console.error('Error:', error));
 
         });
 
@@ -395,14 +397,14 @@ function add_post(content){
         document.querySelector("#posts").append(post);
     })
 
-    
+
 
 }
 
 // Get username
-async function get_username(user_id){
+async function get_username(user_id) {
     console.log(user_id);
-    
+
     const resposne = await fetch(`/username?user_id=${user_id}`);
     const data = await resposne.json();
     return data.username;
@@ -410,14 +412,14 @@ async function get_username(user_id){
 
 // Javascript function to get the current user from django session
 
-function get_current_user(){
+function get_current_user() {
     fetch('/current_user')
-    .then(response => response.json())
-    .then(data => {
-        console.log(`This is the current user: ${data}`);
-        // save the current user to local storage
-        localStorage.setItem('user_id', data);
-    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(`This is the current user: ${data}`);
+            // save the current user to local storage
+            localStorage.setItem('user_id', data);
+        })
 }
 
 
